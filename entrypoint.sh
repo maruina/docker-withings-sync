f888eb2a79bf3e85630bccafaf59c03b314a6acc
Matteo Ruina <matteo.ruina@gmail.com>
2021-05-19 15:00:52 +0200

    Add verbose mode and configurable sleep

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,18 @@
 
 set -eu
 
+sleep="${SLEEP:-900}"
+verbose="${VERBOSE:-false}"
+
+if [[ ${verbose} == "true" ]]; then
+    args="-v"
+    echo "sleep value: ${sleep}"
+    echo "withings-sync arguments: ${args}"
+fi
+
+
 while true; do
-    withings-sync
-    echo "Command completed. Sleeping ${SLEEP} seconds..."
-    sleep "${SLEEP}"
+    withings-sync "${args}"
+    echo "Command completed. Sleeping ${sleep} seconds..."
+    sleep "${sleep}"
 done

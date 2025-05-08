d52ebb13051fc3c450dc13d3a6f80e6ea783c7d4
Matteo Ruina <matteo.ruina@datadoghq.com>
2025-05-08 23:16:00 +0200

    Add logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,8 @@ echo "$(date): Container started"
 
 while true;
 do
+    echo "$(date): Starting withings-sync" >> /var/log/withings-sync.log
     /usr/local/bin/withings-sync -v >> /var/log/withings-sync.log 2>&1
+    echo "$(date): Finished withings-sync" >> /var/log/withings-sync.log
     sleep 21600 # 6h
 done

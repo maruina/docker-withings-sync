95af5d176363968da47a44bb3a6ee5673ea0f8a1
Matteo Ruina <matteo.ruina@gmail.com>
2025-05-22 22:18:21 +0200

    Log in case of error

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,13 @@ echo "$(date): Container started"
 
 while true;
 do
-    echo "$(date): Starting withings-sync" >> /var/log/withings-sync.log
-    /usr/local/bin/withings-sync -v >> /var/log/withings-sync.log 2>&1
-    echo "$(date): Finished withings-sync" >> /var/log/withings-sync.log
+    echo "$(date): Starting withings-sync"
+    if ! /usr/local/bin/withings-sync -v >> /var/log/withings-sync.log 2>&1
+    then
+        echo "$(date): Error occurred during withings-sync"
+        # Avoid logging garmin_password to stdout
+        cat /var/log/withings-sync.log | grep -v garmin_password
+    fi
+    echo "$(date): Finished withings-sync, next run in 6 hours"
     sleep 21600 # 6h
 done
